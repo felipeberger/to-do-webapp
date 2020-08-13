41328c2a15f4bb86fcd6db7a0e79c138538a42b4
felipeberger <feconchaber@gmail.com>
2020-08-13 11:02:01 +1000

    Minor edits to HTML file. Added methods and event handlers for the input bar so the user can add new tasks, which are uploaded to the server for later retrieval

--- application.js
+++ application.js
@@ -1,16 +1,15 @@
-var populateAll = function () {
-  var getAllData = function () {
+// task populating methods
+var populateAll = function() {
+  var getAllData = function() {
     $.ajax({
       type: 'GET',
       url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=177',
       dataType: 'json',
       success: function (response) {
-        // return response
-        // console.log(response);
-        populateAllHelper(response)
+        populateAllHelper(response);
       },
 
-      error: function (request, errorMessage) {
+      error: function(request, errorMessage) {
         console.log(errorMessage);
       }
     });
@@ -18,9 +17,8 @@ var populateAll = function () {
 
   var populateAllHelper = function(elem) {
     elem.tasks.forEach(function (item) {
-      console.log(item['content']);
       $('.task-list').prepend(
-        '<div class="task">' +
+        '<div class="task" id=' + item['id'] + '>' +
           '<hr>' +
           '<i class="far fa-square pr-3"></i>' +
           '<p class="d-inline">' + item['content'] + '</p>' +
@@ -30,6 +28,39 @@ var populateAll = function () {
   };
   getAllData();
 };
+
+var uploadSingleEntry = function(entry) {
+  $.ajax({
+    type: 'POST',
+    url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=177',
+    contentType: 'application/json',
+    dataType: 'json',
+    data: JSON.stringify({
+      task: {
+        content: entry
+      }
+    }),
+    success: function (response, textStatus) {
+      console.log(response);
+    },
+    error: function (request, textStatus, errorMessage) {
+      console.log(errorMessage);
+    }
+  });
+}
+
+var deleteSingleEntry = function(entry) {
+  $.ajax({
+    type: 'DELETE',
+    url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks/' + entry + '?api_key=177',
+    success: function (response, textStatus) {
+      console.log(response);
+    },
+    error: function (request, textStatus, errorMessage) {
+      console.log(errorMessage);
+    }
+  })
+}
 
 // $.ajax({
 //   type: 'POST',
@@ -59,6 +90,29 @@ var populateAll = function () {
 //     console.log(errorMessage);
 //   }
 // })
+
+// event handlers
+$(document).on('keypress', 'input', function() {
+  if (event.which === 13) {
+    var temp = $('input').val();
+    console.log(temp);
+    $('.task-list').append(
+      '<div class="task">' +
+        '<hr>' +
+        '<i class="far fa-square pr-3"></i>' +
+        '<p class="d-inline">' + temp + '</p>' +
+      '</div>'
+    )
+    $('input').val('');
+    uploadSingleEntry(temp);
+  }
+});
+
+$(document).on('click', '.fa-square', function() {
+
+})
+
+
 
 // populate list on DOM load
 $(document).ready(function() {
